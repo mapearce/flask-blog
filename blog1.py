7663756ffe777302b86a04f071586a590addddd7
Michael Pearce <themikepearce@gmail.com>
2016-07-11 22:31:08 -0700

    Added 2 routes

--- blog1.py
+++ blog1.py
@@ -16,5 +16,13 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
+@app.route('/')
+def login():
+	return render_template('login.html')
+
+@app.route('/main')
+def main():
+	return render_template('main.html')
+
 if name == '__main__':
 	app.run(debut=True)
