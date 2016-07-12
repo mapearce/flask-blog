4daaaf3030aa0770957908d62d8bb27b9eb7a6de
Michael Pearce <themikepearce@gmail.com>
2016-07-11 22:25:40 -0700

    Added the config file

---
+++ blog1.py
@@ -0,0 +1,20 @@
+#blog.py - controller
+
+#imports
+from flask import Flask, render_template, request, session, flash, redirect. url_for, g
+import sqlite3
+
+#configuration
+DATABASE = 'blog.db'
+
+app = Flask(__name__)
+
+#pulls in app configuration by looking for UPPERCASE variables
+app.config.from_object(__name__)
+
+#function used for connecting to the DATABASE
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+if name == '__main__':
+	app.run(debut=True)
