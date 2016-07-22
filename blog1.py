3f51571ba0a0d657d9b3b95d0694b98d452578b3
Michael Pearce <themikepearce@gmail.com>
2016-07-21 21:10:11 -0700

    Made the blog, styled it, etc

--- blog1.py
+++ blog1.py
@@ -1,28 +1,74 @@
-#blog.py - controller
-
-#imports
-from flask import Flask, render_template, request, session, flash, redirect. url_for, g
+from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
-#configuration
+#Configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
 
 app = Flask(__name__)
 
-#pulls in app configuration by looking for UPPERCASE variables
+#Pulls in app configuration by looking for UpperCASE variables
 app.config.from_object(__name__)
 
-#function used for connecting to the DATABASE
+#Function used for connecting to the database
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to loggert in feerst.')
+			return redirect(url_for('login'))
+	return wrap
+
+@app.route('/', methods=['GET','POST'])
 def login():
-	return render_template('login.html')
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid credenza.  Please chair again'
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('main'))
+	return render_template('login.html', error=error)
 
 @app.route('/main')
+@login_required
 def main():
-	return render_template('main.html')
+	g.db = connect_db()
+	cur = g.db.execute('SELECT * FROM posts')
+	posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template('main.html', posts=posts)
 
-if name == '__main__':
-	app.run(debut=True)
+@app.route('/logout')
+def logout():
+	session.pop('logged_in',None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+	title = request.form['title']
+	post = request.form['post']
+	if not title or not post:
+		flash("All fields are required homie.  Please try again and be less stupid this time.")
+		return redirect(url_for('main'))
+	else:
+		g.db = connect_db()
+		g.db.execute('INSERT INTO posts (title, post) VALUES (?,?)',[request.form['title'], request.form['post']])
+		g.db.commit()
+		g.db.close()
+		flash('New entry accepted and posted!')
+		return redirect(url_for('main'))
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
